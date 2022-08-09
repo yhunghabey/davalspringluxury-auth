439c847c255699a203c17645ab27634ae51cb93a
Yhunghabey <yhung.habey1994@gmail.com>
2022-08-09 20:22:42 +0100

    clean up

--- app/module/user/service.js
+++ app/module/user/service.js
@@ -77,6 +77,7 @@ export async function login(body) {
     throw err;
   }
 }
+
 export async function getUserById(params) {
   try {
     const user = await User.findById(params.id);
@@ -218,36 +219,6 @@ export async function updateAccountStatus(user, body) {
       });
 
     return successMessage("Account status changed to " + body.status);
-  } catch (err) {
-    throw err;
-  }
-}
-
-export async function merchantOnboard(body) {
-  try {
-    const isExist = await User.findOne({ email: body.email });
-    if (isExist) {
-      throw new ExistsError(`${body.email} already Exist`);
-    }
-    const phoneExist = await User.findOne({ phoneNumber: body.phone });
-    if (phoneExist) {
-      throw new ExistsError("User with Phone Number already exists");
-    }
-
-    let newUser = new User();
-    newUser.firstname = body.firstname;
-    newUser.lastname = body.lastname;
-    newUser.email = body.email.trim();
-    newUser.phoneNumber = body.phoneNumber;
-    newUser.password = body.password;
-    newUser.userType = body.userType.toUpperCase();
-    newUser.permissions = USERTYPE[body.userType.toUpperCase()];
-    await newUser.save();
-    return {
-      success,
-      message: `You have successfully created your account, kindly check your email inbox or spam folder to get verification link.`,
-      data: await setAuth(newUser),
-    };
   } catch (err) {
     throw err;
   }
